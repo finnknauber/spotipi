3a0d70251697e907b52f0e1b0ae9530c4b5ecad6
Finn <finn.knauber@gmail.com>
2024-03-31 16:16:25 +0200

    logger no logging pls

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def spotifycallback():
 
 def reader():
     reader = ExtendedMFRC522()
-    reader.logger.propagate = False
+    reader.READER.logger.propagate = False
     try:
         while True:
             id, text = reader.read_no_block()
