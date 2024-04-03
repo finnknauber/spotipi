eef556595734d19d2349d705ac2ca32d6f7c420e
Finn <finn.knauber@gmail.com>
2024-04-03 17:08:28 +0200

    play success

--- buzzer.py
+++ buzzer.py
@@ -9,18 +9,35 @@ def play_tone(frequency, duration):
     period = 1.0 / frequency
     half_period = period / 2.0
     cycles = int(duration / period)
-    
+
     for _ in range(cycles):
         GPIO.output(18, GPIO.HIGH)
         time.sleep(half_period)
         GPIO.output(18, GPIO.LOW)
         time.sleep(half_period)
 
+#   // play error sound
+#   tone(buzzer, 350, 100);
+#   delay(200);
+#   tone(buzzer, 350, 400);
+#   delay(500);
+
+#   // play notification sound
+#   tone(buzzer, 3000, 250);
+#   delay(300);
+#   tone(buzzer, 2250, 100);
+#   delay(125);
+#   tone(buzzer, 2250, 100);
+
+#   // play cancel sound
+#   tone(buzzer, 500, 250);
+#   delay(300);
+#   tone(buzzer, 300, 150);
 
 def play_success():
-    play_tone(500, 0.5)
-    time.sleep(1)
-    play_tone(1000, 0.5)
+    play_tone(2000, 0.25)
+    time.sleep(0.3)
+    play_tone(2200, 0.15)
     GPIO.output(18, GPIO.LOW)
     GPIO.cleanup()
 
