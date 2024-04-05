c4b178ef176857fc88d689a95643f1b5d8907380
Finn <finn.knauber@gmail.com>
2024-04-05 17:36:51 +0200

    testing rfid

---
+++ RFIDtesting.py
@@ -0,0 +1,36 @@
+from mfrc522 import ExtendedMFRC522
+import RPi.GPIO as GPIO
+import time
+
+
+def writeTag(text):
+    if text:
+        print("Place tag on reader")
+        reader = ExtendedMFRC522(sections=10)
+        try:
+            reader.write(text)
+        finally:
+            GPIO.cleanup()
+        print("Written")
+
+
+def readTag():
+    reader = ExtendedMFRC522()
+    try:
+        while True:
+            id, text = reader.read_no_block()
+            if id:
+                print("Read text:", text)
+    finally:
+        print("cleaning up reader")
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    print("Writing text: 'This is a test' to tag")
+    writeTag("This is a test")
+    time.sleep(3)
+    print("Reading text from tag")
+    readTag()
+    print("Finished")
+
