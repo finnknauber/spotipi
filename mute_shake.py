35b7d2b92bb069ca9c7e56fb100e87b6816ef464
Finn <finn.knauber@gmail.com>
2024-04-08 19:29:45 +0200

    trying

--- mute_shake.py
+++ mute_shake.py
@@ -1,14 +1,14 @@
-import time
+# import time
 import os
 
-def mute_on_shake():
-    input = DigitalInputDevice(4)
-    last_shake = time.time()
-    muted = False
+# input = DigitalInputDevice(4)
+# last_shake = time.time()
+# muted = False
 
-    while True:
-        if input.value:
-            if time.time() - last_shake > 3:
-                last_shake = time.time()
-                print("Mute!")
-                os.system("amixer set Master toggle")
+# while True:
+#     if input.value:
+#         if time.time() - last_shake > 3:
+#             last_shake = time.time()
+#             print("Mute!")
+
+os.system("amixer set Master toggle")
