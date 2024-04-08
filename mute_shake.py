a17553bdfc6f83db6421fd51a0c824c13d4a9718
Finn <finn.knauber@gmail.com>
2024-04-08 19:17:02 +0200

    why

--- mute_shake.py
+++ mute_shake.py
@@ -5,10 +5,11 @@ import os
 def mute_on_shake():
     input = DigitalInputDevice(4)
     last_shake = time.time()
+    muted = False
 
     while True:
         if input.value:
             if time.time() - last_shake > 3:
                 last_shake = time.time()
                 print("Mute!")
-                os.system("amixer -D pulse sset Master mute")
+                os.system("sudo amixer -D pulse sset Master " + ("unmute" if muted else "mute"))
